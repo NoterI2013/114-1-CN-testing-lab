94dee485a7a6b3d385d9e312c5114b5d36e5fa23
NoterI2013 <noteri2013@gmail.com>
2026-04-23 12:40:16 +0000

    Fabonacci Test

--- backend/test/utils.test.ts
+++ backend/test/utils.test.ts
@@ -21,16 +21,34 @@ describe('math utils testing', () => {
 
   describe('fabonacci testing', () => {
     it('should return 1 when n is 1', () => {
-     // TODO: fix the test
-      expect.fail('not implemented')
+      // arrange
+      const n = 1
+      
+      // act
+      const actual = fabonacci(n)
+
+      // assert
+      expect(actual).toBe(1)
     })
     it('should return 1 when n is 2', () => {
-      // TODO: fix the test
-      expect.fail('not implemented')
+      // arrange
+      const n = 2
+      
+      // act
+      const actual = fabonacci(n)
+
+      // assert
+      expect(actual).toBe(1)
     })
     it('should return 2 when n is 3', () => {
-      // TODO: fix the test
-      expect.fail('not implemented')
+      // arrange
+      const n = 3
+      
+      // act
+      const actual = fabonacci(n)
+
+      // assert
+      expect(actual).toBe(2)
     })
   })
 })
